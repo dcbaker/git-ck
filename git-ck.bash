11bd9dcee189a2d736d2237fa58603991497f2c9
Dylan Baker <baker.dylan.c@gmail.com>
2015-09-21 11:22:56 -0700

    git-ck.bash: add some documentation about the completions file

--- git-ck.bash
+++ git-ck.bash
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
+# Bash completions for git-ck
+
 # FIXME: This almost certainly is not distro safe, it works on Arch, and thats
 #        a start
 source /usr/share/git/completion/git-completion.bash
